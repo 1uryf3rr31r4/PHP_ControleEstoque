0c9604f1421c40d83f3b78f37d7e00f6f2cb934f
Iury Ferreira <1uryf3rr31r4@gmail.com>
2021-07-20 14:39:14 -0300

    Código do Entrada de Usuário e Senha

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title>Controle de Estoque</title>
+    </head>
+    <body>
+        <h3>Login</h3>
+        <?php
+            if(isset($_SESSION['nao_autenticado'])):
+        ?>
+            <p>ERRO: Usuário ou senha inválidos.</p>
+        <?php
+            endif;
+            unset($_SESSION['nao_autenticado']);
+        ?>
+        <form action="login.php" method="POST">
+            <input name="usuario" name="text" placeholder="Seu usuário" autofocus="">
+            <input name="senha" type="password" placeholder="Sua senha">
+            <button type="submit">Entrar</button>
+        </form>
+    </body>
+</html>
